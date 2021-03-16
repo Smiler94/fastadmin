e13aca56279866f53afde2d405e65f8511b34ab6
linzhen <273487984@qq.com>
2021-03-16 22:14:09 +0800

    modify

--- public/assets/js/backend/cof_issue_custom.js
+++ public/assets/js/backend/cof_issue_custom.js
@@ -24,6 +24,12 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
         table: {
             first: function () {
                 // 表格1
+                Table.api.init({
+                    extend: {
+                        add_url: 'cof_issue_custom/add?GameId=7',
+                        edit_url: 'cof_issue_custom/edit'
+                    }
+                });
                 var table1 = $("#table1");
                 table1.bootstrapTable({
                     url: 'cof_issue_custom/index' + location.search,
@@ -34,11 +40,10 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                     columns: [
                         [
                             {checkbox: true},
-                            {field: 'IssueCustomId', title: __('IssueCustomid')},
+                            {field: 'IssueCustomId', title: __('IssueCustomid'), operate: false},
                             {field: 'IssueId', title: __('Issueid')},
                             {field: 'OpenCode', title: __('Opencode'), operate: 'LIKE'},
                             {field: 'AddTime', title: __('Addtime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false},
-                            {field: 'operate', title: __('Operate'), table: table1, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                         ]
                     ],
                     queryParams: function(params) {
@@ -53,6 +58,12 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             },
             second: function () {
                 // 表格2
+                Table.api.init({
+                    extend: {
+                        add_url: 'cof_issue_custom/add?GameId=8',
+                        edit_url: 'cof_issue_custom/edit'
+                    }
+                });
                 var table2 = $("#table2");
                 table2.bootstrapTable({
                     url: 'cof_issue_custom/index' + location.search,
@@ -68,7 +79,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                             {field: 'IssueId', title: __('Issueid')},
                             {field: 'OpenCode', title: __('Opencode'), operate: 'LIKE'},
                             {field: 'AddTime', title: __('Addtime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false},
-                            {field: 'operate', title: __('Operate'), table: table2, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                         ]
                     ],
                     queryParams: function(params) {
